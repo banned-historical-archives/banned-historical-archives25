5ea2f0f00ba49252f205b585d2f3d2d6855aeffa
Your Name <you@example.com>
2024-12-19 23:34:59 +0800

    1

---
+++ 66473776-a590-4bbb-aebd-898f67ff7d8d.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "66473776-a590-4bbb-aebd-898f67ff7d8d",
+    "name": "体育战报 1967.7.4",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/66473776-a590-4bbb-aebd-898f67ff7d8d/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/66473776-a590-4bbb-aebd-898f67ff7d8d/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/66473776-a590-4bbb-aebd-898f67ff7d8d/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/66473776-a590-4bbb-aebd-898f67ff7d8d/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】体育战报 1967.7.4",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 4
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "66473776-a590-4bbb-aebd-898f67ff7d8d",
+  "resource_type": "book",
+  "version": 2
+}

92b2fac1ab86cac91cce94beb9b0cd75ddbf62b4
Your Name <you@example.com>
2024-12-20 01:03:16 +0800

    1

---
+++ c918582b-b5c6-4de0-af16-e8d3cd884bde.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "c918582b-b5c6-4de0-af16-e8d3cd884bde",
+    "name": "火线报 1967.7.29",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c918582b-b5c6-4de0-af16-e8d3cd884bde/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c918582b-b5c6-4de0-af16-e8d3cd884bde/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c918582b-b5c6-4de0-af16-e8d3cd884bde/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c918582b-b5c6-4de0-af16-e8d3cd884bde/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】火线报 1967.7.29",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 29
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "c918582b-b5c6-4de0-af16-e8d3cd884bde",
+  "resource_type": "book",
+  "version": 2
+}

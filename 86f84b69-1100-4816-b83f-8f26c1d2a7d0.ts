d428dbf4bc1af1f95ac036752eb254a920547007
Your Name <you@example.com>
2024-12-19 23:32:20 +0800

    1

---
+++ 86f84b69-1100-4816-b83f-8f26c1d2a7d0.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "86f84b69-1100-4816-b83f-8f26c1d2a7d0",
+    "name": "人民前线 1969.11.19",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/86f84b69-1100-4816-b83f-8f26c1d2a7d0/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/86f84b69-1100-4816-b83f-8f26c1d2a7d0/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】人民前线 1969.11.19",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 11,
+            "day": 19
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "86f84b69-1100-4816-b83f-8f26c1d2a7d0",
+  "resource_type": "book",
+  "version": 2
+}

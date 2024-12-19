54d74cc52e17acf5daed87b6467e351599828f10
Your Name <you@example.com>
2024-12-20 00:40:08 +0800

    1

---
+++ 97469340-5307-4068-a80a-e453de2d48d2.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "97469340-5307-4068-a80a-e453de2d48d2",
+    "name": "文革动态 1967.9.19",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/97469340-5307-4068-a80a-e453de2d48d2/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/97469340-5307-4068-a80a-e453de2d48d2/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/97469340-5307-4068-a80a-e453de2d48d2/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】文革动态 1967.9.19",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 19
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "97469340-5307-4068-a80a-e453de2d48d2",
+  "resource_type": "book",
+  "version": 2
+}

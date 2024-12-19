bdf6ffa57f2cc51349423ba3bd93ee0a7e26dc3c
Your Name <you@example.com>
2024-12-19 23:35:26 +0800

    1

---
+++ 12c8dd6e-2779-4b2c-bcf7-7217826105bb.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "12c8dd6e-2779-4b2c-bcf7-7217826105bb",
+    "name": "体育战报 1968.1.29",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/12c8dd6e-2779-4b2c-bcf7-7217826105bb/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/12c8dd6e-2779-4b2c-bcf7-7217826105bb/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/12c8dd6e-2779-4b2c-bcf7-7217826105bb/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/12c8dd6e-2779-4b2c-bcf7-7217826105bb/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】体育战报 1968.1.29",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 1,
+            "day": 29
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "12c8dd6e-2779-4b2c-bcf7-7217826105bb",
+  "resource_type": "book",
+  "version": 2
+}

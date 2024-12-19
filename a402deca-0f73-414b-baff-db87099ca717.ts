14634347388e00d462dd81885c396a85bc818f36
Your Name <you@example.com>
2024-12-19 23:36:53 +0800

    1

---
+++ a402deca-0f73-414b-baff-db87099ca717.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "a402deca-0f73-414b-baff-db87099ca717",
+    "name": "动态 1967.8.17",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a402deca-0f73-414b-baff-db87099ca717/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】动态 1967.8.17",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 17
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "a402deca-0f73-414b-baff-db87099ca717",
+  "resource_type": "book",
+  "version": 2
+}

50cab48d155c1bdb417038307ca4d411d694d085
Your Name <you@example.com>
2024-12-19 23:51:07 +0800

    1

---
+++ d91fd8af-12a0-42e5-ac1d-f4f4dde19552.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "d91fd8af-12a0-42e5-ac1d-f4f4dde19552",
+    "name": "卫东 1967.6.15",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/d91fd8af-12a0-42e5-ac1d-f4f4dde19552/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/d91fd8af-12a0-42e5-ac1d-f4f4dde19552/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/d91fd8af-12a0-42e5-ac1d-f4f4dde19552/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/d91fd8af-12a0-42e5-ac1d-f4f4dde19552/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】卫东 1967.6.15",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 6,
+            "day": 15
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "d91fd8af-12a0-42e5-ac1d-f4f4dde19552",
+  "resource_type": "book",
+  "version": 2
+}

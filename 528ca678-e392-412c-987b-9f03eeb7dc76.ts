58ed22f4b5d73a90068baa509889d8ae8d06ea46
Your Name <you@example.com>
2024-12-19 23:32:03 +0800

    1

---
+++ 528ca678-e392-412c-987b-9f03eeb7dc76.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "528ca678-e392-412c-987b-9f03eeb7dc76",
+    "name": "交大通讯 1967.9.5",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/528ca678-e392-412c-987b-9f03eeb7dc76/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/528ca678-e392-412c-987b-9f03eeb7dc76/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/528ca678-e392-412c-987b-9f03eeb7dc76/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】交大通讯 1967.9.5",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 5
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "528ca678-e392-412c-987b-9f03eeb7dc76",
+  "resource_type": "book",
+  "version": 2
+}

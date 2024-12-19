8a8c00e84b0ac75afb99ec4144b0b05c54d153ca
Your Name <you@example.com>
2024-12-20 00:25:10 +0800

    1

---
+++ 19fcb8e6-e391-42d2-8308-a6f37f07a98a.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "19fcb8e6-e391-42d2-8308-a6f37f07a98a",
+    "name": "快讯 1967.9.13",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/19fcb8e6-e391-42d2-8308-a6f37f07a98a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/19fcb8e6-e391-42d2-8308-a6f37f07a98a/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/19fcb8e6-e391-42d2-8308-a6f37f07a98a/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】快讯 1967.9.13",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 13
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "19fcb8e6-e391-42d2-8308-a6f37f07a98a",
+  "resource_type": "book",
+  "version": 2
+}

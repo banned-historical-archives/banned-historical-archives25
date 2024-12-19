b48262f8975ab44a10c921b5fc3e053c0662ecc6
Your Name <you@example.com>
2024-12-20 00:51:25 +0800

    1

---
+++ a1bf890d-f872-4946-8490-c2d78cc81e06.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "a1bf890d-f872-4946-8490-c2d78cc81e06",
+    "name": "新人大 1967.6.6",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a1bf890d-f872-4946-8490-c2d78cc81e06/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a1bf890d-f872-4946-8490-c2d78cc81e06/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a1bf890d-f872-4946-8490-c2d78cc81e06/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a1bf890d-f872-4946-8490-c2d78cc81e06/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】新人大 1967.6.6",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 6,
+            "day": 6
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "a1bf890d-f872-4946-8490-c2d78cc81e06",
+  "resource_type": "book",
+  "version": 2
+}

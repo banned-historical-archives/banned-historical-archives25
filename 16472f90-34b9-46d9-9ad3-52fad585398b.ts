9f9073558db33e8bcade96fc4597feaadc9b1a6b
Your Name <you@example.com>
2024-12-20 01:35:42 +0800

    1

---
+++ 16472f90-34b9-46d9-9ad3-52fad585398b.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "16472f90-34b9-46d9-9ad3-52fad585398b",
+    "name": "红色风暴 1968.8.5",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/16472f90-34b9-46d9-9ad3-52fad585398b/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/16472f90-34b9-46d9-9ad3-52fad585398b/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/16472f90-34b9-46d9-9ad3-52fad585398b/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/16472f90-34b9-46d9-9ad3-52fad585398b/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】红色风暴 1968.8.5",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 8,
+            "day": 5
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "16472f90-34b9-46d9-9ad3-52fad585398b",
+  "resource_type": "book",
+  "version": 2
+}

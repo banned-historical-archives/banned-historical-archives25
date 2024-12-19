8c292cf586ad3405614f4b86c793cee8826cc67e
Your Name <you@example.com>
2024-12-20 00:37:11 +0800

    1

---
+++ 23dd73e8-d3df-4e4c-a988-f7fe44948bc9.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "23dd73e8-d3df-4e4c-a988-f7fe44948bc9",
+    "name": "文艺 1968.6.14",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/23dd73e8-d3df-4e4c-a988-f7fe44948bc9/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/23dd73e8-d3df-4e4c-a988-f7fe44948bc9/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/23dd73e8-d3df-4e4c-a988-f7fe44948bc9/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/23dd73e8-d3df-4e4c-a988-f7fe44948bc9/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】文艺 1968.6.14",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 6,
+            "day": 14
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "23dd73e8-d3df-4e4c-a988-f7fe44948bc9",
+  "resource_type": "book",
+  "version": 2
+}

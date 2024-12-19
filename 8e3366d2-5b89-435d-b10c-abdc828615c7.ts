a2212b030033b08386a2070d48a25264909ac173
Your Name <you@example.com>
2024-12-19 23:10:00 +0800

    1

---
+++ 8e3366d2-5b89-435d-b10c-abdc828615c7.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "8e3366d2-5b89-435d-b10c-abdc828615c7",
+    "name": "三司硬革联 1967.9.21",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e3366d2-5b89-435d-b10c-abdc828615c7/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e3366d2-5b89-435d-b10c-abdc828615c7/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e3366d2-5b89-435d-b10c-abdc828615c7/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e3366d2-5b89-435d-b10c-abdc828615c7/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】三司硬革联 1967.9.21",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 21
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "8e3366d2-5b89-435d-b10c-abdc828615c7",
+  "resource_type": "book",
+  "version": 2
+}

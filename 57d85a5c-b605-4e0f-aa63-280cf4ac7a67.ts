3b5991f9d86c8835a2b7b6357d44a07f3246d20a
Your Name <you@example.com>
2024-12-20 00:03:18 +0800

    1

---
+++ 57d85a5c-b605-4e0f-aa63-280cf4ac7a67.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "57d85a5c-b605-4e0f-aa63-280cf4ac7a67",
+    "name": "大会通讯 1968.3.20",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/57d85a5c-b605-4e0f-aa63-280cf4ac7a67/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/57d85a5c-b605-4e0f-aa63-280cf4ac7a67/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】大会通讯 1968.3.20",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 3,
+            "day": 20
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "57d85a5c-b605-4e0f-aa63-280cf4ac7a67",
+  "resource_type": "book",
+  "version": 2
+}

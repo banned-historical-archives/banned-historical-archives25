d239d9e3f69bb32921828e91526098dbfeb95821
Your Name <you@example.com>
2024-12-19 23:34:13 +0800

    1

---
+++ cc53538f-dcf2-42b3-a54b-e49676c0290a.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "cc53538f-dcf2-42b3-a54b-e49676c0290a",
+    "name": "体育 1969.5.1",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/cc53538f-dcf2-42b3-a54b-e49676c0290a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/cc53538f-dcf2-42b3-a54b-e49676c0290a/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/cc53538f-dcf2-42b3-a54b-e49676c0290a/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/cc53538f-dcf2-42b3-a54b-e49676c0290a/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】体育 1969.5.1",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 5,
+            "day": 1
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "cc53538f-dcf2-42b3-a54b-e49676c0290a",
+  "resource_type": "book",
+  "version": 2
+}

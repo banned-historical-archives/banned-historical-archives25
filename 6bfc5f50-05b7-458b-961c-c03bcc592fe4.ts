80edb87ed99d74ac3cb570c26dd33f24e65c47c0
Your Name <you@example.com>
2024-12-20 00:17:10 +0800

    1

---
+++ 6bfc5f50-05b7-458b-961c-c03bcc592fe4.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "6bfc5f50-05b7-458b-961c-c03bcc592fe4",
+    "name": "工人造反报 1967.3.4",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6bfc5f50-05b7-458b-961c-c03bcc592fe4/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6bfc5f50-05b7-458b-961c-c03bcc592fe4/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6bfc5f50-05b7-458b-961c-c03bcc592fe4/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6bfc5f50-05b7-458b-961c-c03bcc592fe4/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工人造反报 1967.3.4",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 3,
+            "day": 4
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "6bfc5f50-05b7-458b-961c-c03bcc592fe4",
+  "resource_type": "book",
+  "version": 2
+}

4c2241439c3a150ed1a88919414cfb5e177cc44c
Your Name <you@example.com>
2024-12-20 00:57:23 +0800

    1

---
+++ 98d3988b-969d-4ec0-aa49-5c56fea81a3e.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "98d3988b-969d-4ec0-aa49-5c56fea81a3e",
+    "name": "民兵 1968.5.17",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/98d3988b-969d-4ec0-aa49-5c56fea81a3e/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/98d3988b-969d-4ec0-aa49-5c56fea81a3e/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/98d3988b-969d-4ec0-aa49-5c56fea81a3e/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/98d3988b-969d-4ec0-aa49-5c56fea81a3e/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】民兵 1968.5.17",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 5,
+            "day": 17
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "98d3988b-969d-4ec0-aa49-5c56fea81a3e",
+  "resource_type": "book",
+  "version": 2
+}

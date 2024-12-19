3a5c60812b05a3c4733fe30688a662fa120ba68a
Your Name <you@example.com>
2024-12-20 00:30:12 +0800

    1

---
+++ 1737a5e8-a286-43b8-be97-f2d9390b15ab.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "1737a5e8-a286-43b8-be97-f2d9390b15ab",
+    "name": "抗大 1967.6.15",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1737a5e8-a286-43b8-be97-f2d9390b15ab/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1737a5e8-a286-43b8-be97-f2d9390b15ab/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1737a5e8-a286-43b8-be97-f2d9390b15ab/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1737a5e8-a286-43b8-be97-f2d9390b15ab/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】抗大 1967.6.15",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 6,
+            "day": 15
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "1737a5e8-a286-43b8-be97-f2d9390b15ab",
+  "resource_type": "book",
+  "version": 2
+}

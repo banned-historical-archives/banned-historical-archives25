25ee5eac4f234f2e0a9660a64173317a417bd7b7
Your Name <you@example.com>
2024-12-20 00:06:04 +0800

    1

---
+++ 285a4e84-3ea1-4d22-82f4-aa77cc3e92bb.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "285a4e84-3ea1-4d22-82f4-aa77cc3e92bb",
+    "name": "山东民兵 1968.9.20",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/285a4e84-3ea1-4d22-82f4-aa77cc3e92bb/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/285a4e84-3ea1-4d22-82f4-aa77cc3e92bb/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/285a4e84-3ea1-4d22-82f4-aa77cc3e92bb/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】山东民兵 1968.9.20",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 9,
+            "day": 20
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "285a4e84-3ea1-4d22-82f4-aa77cc3e92bb",
+  "resource_type": "book",
+  "version": 2
+}

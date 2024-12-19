11484fee6eb8ad8fc1c15ccfed14edc7931fa82c
Your Name <you@example.com>
2024-12-20 02:05:44 +0800

    1

---
+++ e6ee758d-e5bf-44c5-b4c1-faa11403ec4b.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "e6ee758d-e5bf-44c5-b4c1-faa11403ec4b",
+    "name": "纺织动态 1967.6.27",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e6ee758d-e5bf-44c5-b4c1-faa11403ec4b/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e6ee758d-e5bf-44c5-b4c1-faa11403ec4b/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e6ee758d-e5bf-44c5-b4c1-faa11403ec4b/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】纺织动态 1967.6.27",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 6,
+            "day": 27
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "e6ee758d-e5bf-44c5-b4c1-faa11403ec4b",
+  "resource_type": "book",
+  "version": 2
+}

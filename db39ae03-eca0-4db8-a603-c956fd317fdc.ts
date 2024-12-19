8b2b50591eb7cb666c84e86b7928b93af3fee60e
Your Name <you@example.com>
2024-12-20 00:36:37 +0800

    1

---
+++ db39ae03-eca0-4db8-a603-c956fd317fdc.ts
@@ -0,0 +1,47 @@
+export default {
+  "entity": {
+    "id": "db39ae03-eca0-4db8-a603-c956fd317fdc",
+    "name": "文艺 1968.3.14",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/db39ae03-eca0-4db8-a603-c956fd317fdc/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/db39ae03-eca0-4db8-a603-c956fd317fdc/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/db39ae03-eca0-4db8-a603-c956fd317fdc/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/db39ae03-eca0-4db8-a603-c956fd317fdc/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/db39ae03-eca0-4db8-a603-c956fd317fdc/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/db39ae03-eca0-4db8-a603-c956fd317fdc/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/db39ae03-eca0-4db8-a603-c956fd317fdc/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/db39ae03-eca0-4db8-a603-c956fd317fdc/8.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】文艺 1968.3.14",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 8,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 3,
+            "day": 14
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "db39ae03-eca0-4db8-a603-c956fd317fdc",
+  "resource_type": "book",
+  "version": 2
+}

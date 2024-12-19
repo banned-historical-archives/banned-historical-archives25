2cff1eed86bc2a2432170b0b71e2fddc0b7f0a70
Your Name <you@example.com>
2024-12-19 23:55:04 +0800

    1

---
+++ d5eed3e4-06d6-4d26-9564-193a6245a3e1.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "d5eed3e4-06d6-4d26-9564-193a6245a3e1",
+    "name": "反到底 1967.7.27",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/d5eed3e4-06d6-4d26-9564-193a6245a3e1/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/d5eed3e4-06d6-4d26-9564-193a6245a3e1/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】反到底 1967.7.27",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 27
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "d5eed3e4-06d6-4d26-9564-193a6245a3e1",
+  "resource_type": "book",
+  "version": 2
+}

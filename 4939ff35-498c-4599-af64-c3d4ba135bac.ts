04c088c50d2e2388d48480d9955437c535310d0f
Your Name <you@example.com>
2024-12-19 23:59:02 +0800

    1

---
+++ 4939ff35-498c-4599-af64-c3d4ba135bac.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "4939ff35-498c-4599-af64-c3d4ba135bac",
+    "name": "哈工大 1974.3.9",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4939ff35-498c-4599-af64-c3d4ba135bac/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4939ff35-498c-4599-af64-c3d4ba135bac/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4939ff35-498c-4599-af64-c3d4ba135bac/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4939ff35-498c-4599-af64-c3d4ba135bac/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】哈工大 1974.3.9",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 3,
+            "day": 9
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "4939ff35-498c-4599-af64-c3d4ba135bac",
+  "resource_type": "book",
+  "version": 2
+}

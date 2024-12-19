4c0c921e3fd7a34b2d2e9b16686b16b7f3a0568e
Your Name <you@example.com>
2024-12-20 01:03:36 +0800

    1

---
+++ 2c3685b8-f890-4525-ba77-5e9a1be03205.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "2c3685b8-f890-4525-ba77-5e9a1be03205",
+    "name": "炮声隆 1967.7.4",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2c3685b8-f890-4525-ba77-5e9a1be03205/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2c3685b8-f890-4525-ba77-5e9a1be03205/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2c3685b8-f890-4525-ba77-5e9a1be03205/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2c3685b8-f890-4525-ba77-5e9a1be03205/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】炮声隆 1967.7.4",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 4
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "2c3685b8-f890-4525-ba77-5e9a1be03205",
+  "resource_type": "book",
+  "version": 2
+}

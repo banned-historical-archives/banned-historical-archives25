537a562e9e5f32e07a139c8dfb95503eeccb9d03
Your Name <you@example.com>
2024-12-20 01:10:37 +0800

    1

---
+++ 2fb0e110-4a1a-4eff-8453-8eb020a247ed.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "2fb0e110-4a1a-4eff-8453-8eb020a247ed",
+    "name": "红卫通讯 1967.9.21",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2fb0e110-4a1a-4eff-8453-8eb020a247ed/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2fb0e110-4a1a-4eff-8453-8eb020a247ed/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2fb0e110-4a1a-4eff-8453-8eb020a247ed/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】红卫通讯 1967.9.21",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 21
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "2fb0e110-4a1a-4eff-8453-8eb020a247ed",
+  "resource_type": "book",
+  "version": 2
+}

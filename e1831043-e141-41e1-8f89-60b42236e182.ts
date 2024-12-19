d80e296bbab7e9419a30ba69fbebab668c199d38
Your Name <you@example.com>
2024-12-20 00:05:10 +0800

    1

---
+++ e1831043-e141-41e1-8f89-60b42236e182.ts
@@ -0,0 +1,52 @@
+export default {
+  "entity": {
+    "id": "e1831043-e141-41e1-8f89-60b42236e182",
+    "name": "大批判 1969.5.10",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1831043-e141-41e1-8f89-60b42236e182/13.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】大批判 1969.5.10",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 13,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 5,
+            "day": 10
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "e1831043-e141-41e1-8f89-60b42236e182",
+  "resource_type": "book",
+  "version": 2
+}

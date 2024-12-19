07af881bf50f759f58cf48b7151e4ee5784bd4bb
Your Name <you@example.com>
2024-12-19 23:36:37 +0800

    1

---
+++ 1fd07034-0374-495c-beea-82f71f7e8651.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "1fd07034-0374-495c-beea-82f71f7e8651",
+    "name": "军内文革通讯 1967.9.10",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1fd07034-0374-495c-beea-82f71f7e8651/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1fd07034-0374-495c-beea-82f71f7e8651/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1fd07034-0374-495c-beea-82f71f7e8651/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】军内文革通讯 1967.9.10",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 10
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "1fd07034-0374-495c-beea-82f71f7e8651",
+  "resource_type": "book",
+  "version": 2
+}

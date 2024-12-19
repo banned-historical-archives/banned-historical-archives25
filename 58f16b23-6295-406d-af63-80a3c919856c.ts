04a5f2774189e952233503f5284db2eb9ba0b61d
Your Name <you@example.com>
2024-12-20 02:07:02 +0800

    1

---
+++ 58f16b23-6295-406d-af63-80a3c919856c.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "58f16b23-6295-406d-af63-80a3c919856c",
+    "name": "职教战报 1967.8.31",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/58f16b23-6295-406d-af63-80a3c919856c/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/58f16b23-6295-406d-af63-80a3c919856c/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/58f16b23-6295-406d-af63-80a3c919856c/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/58f16b23-6295-406d-af63-80a3c919856c/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】职教战报 1967.8.31",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 31
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "58f16b23-6295-406d-af63-80a3c919856c",
+  "resource_type": "book",
+  "version": 2
+}

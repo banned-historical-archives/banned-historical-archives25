06712c40e9b63d16e8782cc1c0840ad747821f89
Your Name <you@example.com>
2024-12-20 01:05:09 +0800

    1

---
+++ a5b51005-10d5-49a5-bf62-adaee56dfb96.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "a5b51005-10d5-49a5-bf62-adaee56dfb96",
+    "name": "简报 1967.8.23",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a5b51005-10d5-49a5-bf62-adaee56dfb96/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】简报 1967.8.23",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 23
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "a5b51005-10d5-49a5-bf62-adaee56dfb96",
+  "resource_type": "book",
+  "version": 2
+}

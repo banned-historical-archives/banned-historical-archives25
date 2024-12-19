15520255dba1805bb810e97492567dc4944832c9
Your Name <you@example.com>
2024-12-20 00:59:17 +0800

    1

---
+++ 1ee677a4-4020-4c86-b1c9-caf335e747a3.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "1ee677a4-4020-4c86-b1c9-caf335e747a3",
+    "name": "江西广播 1968.10.23",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1ee677a4-4020-4c86-b1c9-caf335e747a3/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1ee677a4-4020-4c86-b1c9-caf335e747a3/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1ee677a4-4020-4c86-b1c9-caf335e747a3/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1ee677a4-4020-4c86-b1c9-caf335e747a3/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】江西广播 1968.10.23",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 10,
+            "day": 23
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "1ee677a4-4020-4c86-b1c9-caf335e747a3",
+  "resource_type": "book",
+  "version": 2
+}

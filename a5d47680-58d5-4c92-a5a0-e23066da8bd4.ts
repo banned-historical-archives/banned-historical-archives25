7e67dd268c6381a2bcb0c3ba2b26d8d4271d5c46
Your Name <you@example.com>
2024-12-20 02:13:20 +0800

    1

---
+++ a5d47680-58d5-4c92-a5a0-e23066da8bd4.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "a5d47680-58d5-4c92-a5a0-e23066da8bd4",
+    "name": "革命造反报 1967.6.15",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a5d47680-58d5-4c92-a5a0-e23066da8bd4/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a5d47680-58d5-4c92-a5a0-e23066da8bd4/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a5d47680-58d5-4c92-a5a0-e23066da8bd4/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】革命造反报 1967.6.15",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 6,
+            "day": 15
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "a5d47680-58d5-4c92-a5a0-e23066da8bd4",
+  "resource_type": "book",
+  "version": 2
+}

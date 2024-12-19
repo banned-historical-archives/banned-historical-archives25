07f7d1d2937baf272b1ecce135d5a5081ef81222
Your Name <you@example.com>
2024-12-20 02:14:03 +0800

    1

---
+++ 4b0c9fbc-bb6c-4031-a271-2a973e80c070.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "4b0c9fbc-bb6c-4031-a271-2a973e80c070",
+    "name": "风雷激 1967.2.2",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4b0c9fbc-bb6c-4031-a271-2a973e80c070/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4b0c9fbc-bb6c-4031-a271-2a973e80c070/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】风雷激 1967.2.2",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 2,
+            "day": 2
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "4b0c9fbc-bb6c-4031-a271-2a973e80c070",
+  "resource_type": "book",
+  "version": 2
+}

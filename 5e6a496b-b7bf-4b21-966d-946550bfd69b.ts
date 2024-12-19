f6c357b3915845b675011149f3e6611519602343
Your Name <you@example.com>
2024-12-20 01:13:11 +0800

    1

---
+++ 5e6a496b-b7bf-4b21-966d-946550bfd69b.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "5e6a496b-b7bf-4b21-966d-946550bfd69b",
+    "name": "红炮兵 1967.9.1",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/5e6a496b-b7bf-4b21-966d-946550bfd69b/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/5e6a496b-b7bf-4b21-966d-946550bfd69b/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/5e6a496b-b7bf-4b21-966d-946550bfd69b/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/5e6a496b-b7bf-4b21-966d-946550bfd69b/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】红炮兵 1967.9.1",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 1
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "5e6a496b-b7bf-4b21-966d-946550bfd69b",
+  "resource_type": "book",
+  "version": 2
+}

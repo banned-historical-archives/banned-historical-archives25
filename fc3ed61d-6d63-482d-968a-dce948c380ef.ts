4276d0a868f65944c9a3b0a91590fbc0ff67c38a
Your Name <you@example.com>
2024-12-20 00:16:16 +0800

    1

---
+++ fc3ed61d-6d63-482d-968a-dce948c380ef.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "fc3ed61d-6d63-482d-968a-dce948c380ef",
+    "name": "工人造反 1970.10.15",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/fc3ed61d-6d63-482d-968a-dce948c380ef/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/fc3ed61d-6d63-482d-968a-dce948c380ef/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/fc3ed61d-6d63-482d-968a-dce948c380ef/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/fc3ed61d-6d63-482d-968a-dce948c380ef/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工人造反 1970.10.15",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1970,
+            "month": 10,
+            "day": 15
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "fc3ed61d-6d63-482d-968a-dce948c380ef",
+  "resource_type": "book",
+  "version": 2
+}

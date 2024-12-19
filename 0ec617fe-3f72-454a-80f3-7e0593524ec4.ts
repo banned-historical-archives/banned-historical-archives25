c9ce0d878fb2b69186659fbc25def2549166ee98
Your Name <you@example.com>
2024-12-20 02:14:45 +0800

    1

---
+++ 0ec617fe-3f72-454a-80f3-7e0593524ec4.ts
@@ -0,0 +1,51 @@
+export default {
+  "entity": {
+    "id": "0ec617fe-3f72-454a-80f3-7e0593524ec4",
+    "name": "风雷激 1967.4.30",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ec617fe-3f72-454a-80f3-7e0593524ec4/12.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】风雷激 1967.4.30",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 12,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 4,
+            "day": 30
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "0ec617fe-3f72-454a-80f3-7e0593524ec4",
+  "resource_type": "book",
+  "version": 2
+}

30282cf316f26e1632cd8dfddc2b02fd44002d0d
Your Name <you@example.com>
2024-12-20 02:17:35 +0800

    1

---
+++ 068a6c90-d5b7-4152-bb9e-e91113485c77.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "068a6c90-d5b7-4152-bb9e-e91113485c77",
+    "name": "首都红卫兵 1967.2.8",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/068a6c90-d5b7-4152-bb9e-e91113485c77/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/068a6c90-d5b7-4152-bb9e-e91113485c77/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/068a6c90-d5b7-4152-bb9e-e91113485c77/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/068a6c90-d5b7-4152-bb9e-e91113485c77/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】首都红卫兵 1967.2.8",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 2,
+            "day": 8
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "068a6c90-d5b7-4152-bb9e-e91113485c77",
+  "resource_type": "book",
+  "version": 2
+}

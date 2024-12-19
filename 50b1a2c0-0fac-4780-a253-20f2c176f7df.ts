8b2fb07b21fea321485fb062b4695adde659db75
Your Name <you@example.com>
2024-12-20 00:21:34 +0800

    1

---
+++ 50b1a2c0-0fac-4780-a253-20f2c176f7df.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "50b1a2c0-0fac-4780-a253-20f2c176f7df",
+    "name": "工人造反报 1969.4.24",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/50b1a2c0-0fac-4780-a253-20f2c176f7df/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/50b1a2c0-0fac-4780-a253-20f2c176f7df/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/50b1a2c0-0fac-4780-a253-20f2c176f7df/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/50b1a2c0-0fac-4780-a253-20f2c176f7df/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工人造反报 1969.4.24",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 4,
+            "day": 24
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "50b1a2c0-0fac-4780-a253-20f2c176f7df",
+  "resource_type": "book",
+  "version": 2
+}

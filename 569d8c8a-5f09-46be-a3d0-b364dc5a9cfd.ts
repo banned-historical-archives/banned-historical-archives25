d693a3c9ed9df67db8844a574ba6978e478940c5
Your Name <you@example.com>
2024-12-20 02:07:26 +0800

    1

---
+++ 569d8c8a-5f09-46be-a3d0-b364dc5a9cfd.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "569d8c8a-5f09-46be-a3d0-b364dc5a9cfd",
+    "name": "财贸战报 1967.10.10",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/569d8c8a-5f09-46be-a3d0-b364dc5a9cfd/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/569d8c8a-5f09-46be-a3d0-b364dc5a9cfd/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/569d8c8a-5f09-46be-a3d0-b364dc5a9cfd/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/569d8c8a-5f09-46be-a3d0-b364dc5a9cfd/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】财贸战报 1967.10.10",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 10,
+            "day": 10
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "569d8c8a-5f09-46be-a3d0-b364dc5a9cfd",
+  "resource_type": "book",
+  "version": 2
+}

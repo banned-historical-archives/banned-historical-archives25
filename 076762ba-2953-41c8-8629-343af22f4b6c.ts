078c95983d40d49103a1a0b43730e5b843665294
Your Name <you@example.com>
2024-12-20 00:54:17 +0800

    1

---
+++ 076762ba-2953-41c8-8629-343af22f4b6c.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "076762ba-2953-41c8-8629-343af22f4b6c",
+    "name": "新江苏简讯 1967.8.7",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/076762ba-2953-41c8-8629-343af22f4b6c/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/076762ba-2953-41c8-8629-343af22f4b6c/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/076762ba-2953-41c8-8629-343af22f4b6c/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】新江苏简讯 1967.8.7",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 7
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "076762ba-2953-41c8-8629-343af22f4b6c",
+  "resource_type": "book",
+  "version": 2
+}

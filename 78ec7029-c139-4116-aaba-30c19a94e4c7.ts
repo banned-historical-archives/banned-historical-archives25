213fb05941f91ac3f2b16acd64d08f6cff2d48c2
Your Name <you@example.com>
2024-12-20 01:00:10 +0800

    1

---
+++ 78ec7029-c139-4116-aaba-30c19a94e4c7.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "78ec7029-c139-4116-aaba-30c19a94e4c7",
+    "name": "浙江大批判 1969.3.31",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/78ec7029-c139-4116-aaba-30c19a94e4c7/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/78ec7029-c139-4116-aaba-30c19a94e4c7/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/78ec7029-c139-4116-aaba-30c19a94e4c7/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/78ec7029-c139-4116-aaba-30c19a94e4c7/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】浙江大批判 1969.3.31",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 3,
+            "day": 31
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "78ec7029-c139-4116-aaba-30c19a94e4c7",
+  "resource_type": "book",
+  "version": 2
+}

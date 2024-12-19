c0e6e716739d83e0cafbdaf624f42e2cc72eec38
Your Name <you@example.com>
2024-12-20 01:12:47 +0800

    1

---
+++ 1868f924-7d6a-476a-8d75-89b8491cc5a4.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "1868f924-7d6a-476a-8d75-89b8491cc5a4",
+    "name": "红旗 1967.9.7",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1868f924-7d6a-476a-8d75-89b8491cc5a4/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1868f924-7d6a-476a-8d75-89b8491cc5a4/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1868f924-7d6a-476a-8d75-89b8491cc5a4/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1868f924-7d6a-476a-8d75-89b8491cc5a4/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】红旗 1967.9.7",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 7
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "1868f924-7d6a-476a-8d75-89b8491cc5a4",
+  "resource_type": "book",
+  "version": 2
+}

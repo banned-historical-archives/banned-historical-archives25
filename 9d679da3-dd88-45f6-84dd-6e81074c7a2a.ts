7ea88e5190feda43013f7cab52a5bb2c0f079dac
Your Name <you@example.com>
2024-12-20 00:26:54 +0800

    1

---
+++ 9d679da3-dd88-45f6-84dd-6e81074c7a2a.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "9d679da3-dd88-45f6-84dd-6e81074c7a2a",
+    "name": "打倒李井泉 1967.7.1",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/9d679da3-dd88-45f6-84dd-6e81074c7a2a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/9d679da3-dd88-45f6-84dd-6e81074c7a2a/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/9d679da3-dd88-45f6-84dd-6e81074c7a2a/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/9d679da3-dd88-45f6-84dd-6e81074c7a2a/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】打倒李井泉 1967.7.1",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 1
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "9d679da3-dd88-45f6-84dd-6e81074c7a2a",
+  "resource_type": "book",
+  "version": 2
+}

7d68e11fe6f41c96eadaf4674f40ce3cfb696402
Your Name <you@example.com>
2024-12-19 23:15:21 +0800

    1

---
+++ a68bffd1-c9cd-4575-9763-7e442c132aaf.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "a68bffd1-c9cd-4575-9763-7e442c132aaf",
+    "name": "813红卫兵 1967.4.17",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a68bffd1-c9cd-4575-9763-7e442c132aaf/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a68bffd1-c9cd-4575-9763-7e442c132aaf/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】813红卫兵 1967.4.17",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 4,
+            "day": 17
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "a68bffd1-c9cd-4575-9763-7e442c132aaf",
+  "resource_type": "book",
+  "version": 2
+}

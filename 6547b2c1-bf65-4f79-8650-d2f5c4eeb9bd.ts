6f4368e47d7a90feb4c6339c2fb6631b44aa73fa
Your Name <you@example.com>
2024-12-20 00:23:56 +0800

    1

---
+++ 6547b2c1-bf65-4f79-8650-d2f5c4eeb9bd.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "6547b2c1-bf65-4f79-8650-d2f5c4eeb9bd",
+    "name": "工人造反报 1967.9.12",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6547b2c1-bf65-4f79-8650-d2f5c4eeb9bd/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6547b2c1-bf65-4f79-8650-d2f5c4eeb9bd/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6547b2c1-bf65-4f79-8650-d2f5c4eeb9bd/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6547b2c1-bf65-4f79-8650-d2f5c4eeb9bd/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工人造反报 1967.9.12",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 12
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "6547b2c1-bf65-4f79-8650-d2f5c4eeb9bd",
+  "resource_type": "book",
+  "version": 2
+}

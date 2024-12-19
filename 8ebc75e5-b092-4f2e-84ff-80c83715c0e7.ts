b72e4ade5ea6a722b50ce184dbf59d481f59aff0
Your Name <you@example.com>
2024-12-20 00:09:42 +0800

    1

---
+++ 8ebc75e5-b092-4f2e-84ff-80c83715c0e7.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "8ebc75e5-b092-4f2e-84ff-80c83715c0e7",
+    "name": "山东红卫兵 1967.8.1",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8ebc75e5-b092-4f2e-84ff-80c83715c0e7/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8ebc75e5-b092-4f2e-84ff-80c83715c0e7/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8ebc75e5-b092-4f2e-84ff-80c83715c0e7/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8ebc75e5-b092-4f2e-84ff-80c83715c0e7/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】山东红卫兵 1967.8.1",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 1
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "8ebc75e5-b092-4f2e-84ff-80c83715c0e7",
+  "resource_type": "book",
+  "version": 2
+}

032a8c6670fd1e559fc0c98e275fa1146cf920a8
Your Name <you@example.com>
2024-12-19 23:52:15 +0800

    1

---
+++ 7366c0e3-777f-4d53-baf5-11b934152002.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "7366c0e3-777f-4d53-baf5-11b934152002",
+    "name": "卫东 1967.7.2",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/7366c0e3-777f-4d53-baf5-11b934152002/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/7366c0e3-777f-4d53-baf5-11b934152002/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】卫东 1967.7.2",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 2
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "7366c0e3-777f-4d53-baf5-11b934152002",
+  "resource_type": "book",
+  "version": 2
+}

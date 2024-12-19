a5ff168eaaff58bb045c50ec859e103a5e80a0fd
Your Name <you@example.com>
2024-12-19 23:36:02 +0800

    1

---
+++ 223a6c8d-b189-4b3f-8775-bee15f3faa6e.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "223a6c8d-b189-4b3f-8775-bee15f3faa6e",
+    "name": "兵团战报 1967.5.20",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/223a6c8d-b189-4b3f-8775-bee15f3faa6e/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/223a6c8d-b189-4b3f-8775-bee15f3faa6e/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】兵团战报 1967.5.20",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 5,
+            "day": 20
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "223a6c8d-b189-4b3f-8775-bee15f3faa6e",
+  "resource_type": "book",
+  "version": 2
+}

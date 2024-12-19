012cedf11d6725e0c7fea3584effdd4d039105f8
Your Name <you@example.com>
2024-12-20 00:59:40 +0800

    1

---
+++ f035cbdf-0e97-4d89-a42b-650e03068c84.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "f035cbdf-0e97-4d89-a42b-650e03068c84",
+    "name": "沪农战线 1967.12.17",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/f035cbdf-0e97-4d89-a42b-650e03068c84/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/f035cbdf-0e97-4d89-a42b-650e03068c84/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/f035cbdf-0e97-4d89-a42b-650e03068c84/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】沪农战线 1967.12.17",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 12,
+            "day": 17
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "f035cbdf-0e97-4d89-a42b-650e03068c84",
+  "resource_type": "book",
+  "version": 2
+}

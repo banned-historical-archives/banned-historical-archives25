6f2304f55ba1e95c0f43ed7ec33f26d2204fc8d5
Your Name <you@example.com>
2024-12-19 23:33:09 +0800

    1

---
+++ 0d594978-1b39-4d3b-9329-946f0b06f394.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "0d594978-1b39-4d3b-9329-946f0b06f394",
+    "name": "低压电气工人 1968.1.28",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0d594978-1b39-4d3b-9329-946f0b06f394/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0d594978-1b39-4d3b-9329-946f0b06f394/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0d594978-1b39-4d3b-9329-946f0b06f394/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0d594978-1b39-4d3b-9329-946f0b06f394/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】低压电气工人 1968.1.28",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 1,
+            "day": 28
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "0d594978-1b39-4d3b-9329-946f0b06f394",
+  "resource_type": "book",
+  "version": 2
+}

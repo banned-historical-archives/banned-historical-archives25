62d86f9dc3ac368e841ace39dbe76806b8c4a121
Your Name <you@example.com>
2024-12-20 00:23:34 +0800

    1

---
+++ eaf2be58-8faf-4b1a-a80f-8592d81e648d.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "eaf2be58-8faf-4b1a-a80f-8592d81e648d",
+    "name": "工人造反报 1970.10.1",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/eaf2be58-8faf-4b1a-a80f-8592d81e648d/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/eaf2be58-8faf-4b1a-a80f-8592d81e648d/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/eaf2be58-8faf-4b1a-a80f-8592d81e648d/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/eaf2be58-8faf-4b1a-a80f-8592d81e648d/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工人造反报 1970.10.1",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1970,
+            "month": 10,
+            "day": 1
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "eaf2be58-8faf-4b1a-a80f-8592d81e648d",
+  "resource_type": "book",
+  "version": 2
+}

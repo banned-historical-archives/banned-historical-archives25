3bf201e9a2bb72ca4a581ce52b2cbd9c4a2aac3c
Your Name <you@example.com>
2024-12-19 23:09:33 +0800

    1

---
+++ 58fc449a-80e1-4c6b-bd5d-6c0266b3804d.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "58fc449a-80e1-4c6b-bd5d-6c0266b3804d",
+    "name": "823战报 1967.8.1",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/58fc449a-80e1-4c6b-bd5d-6c0266b3804d/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/58fc449a-80e1-4c6b-bd5d-6c0266b3804d/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/58fc449a-80e1-4c6b-bd5d-6c0266b3804d/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/58fc449a-80e1-4c6b-bd5d-6c0266b3804d/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】823战报 1967.8.1",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 1
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "58fc449a-80e1-4c6b-bd5d-6c0266b3804d",
+  "resource_type": "book",
+  "version": 2
+}

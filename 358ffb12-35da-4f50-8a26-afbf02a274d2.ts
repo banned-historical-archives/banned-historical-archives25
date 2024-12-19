79ae6d4a8e545f8847890c6af423e3c714210994
Your Name <you@example.com>
2024-12-19 23:57:44 +0800

    1

---
+++ 358ffb12-35da-4f50-8a26-afbf02a274d2.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "358ffb12-35da-4f50-8a26-afbf02a274d2",
+    "name": "反逆流 1967.8.12",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/358ffb12-35da-4f50-8a26-afbf02a274d2/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/358ffb12-35da-4f50-8a26-afbf02a274d2/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/358ffb12-35da-4f50-8a26-afbf02a274d2/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/358ffb12-35da-4f50-8a26-afbf02a274d2/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】反逆流 1967.8.12",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 12
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "358ffb12-35da-4f50-8a26-afbf02a274d2",
+  "resource_type": "book",
+  "version": 2
+}

ee5071874b2b63b74dc371e37d6cd49fc7e88538
Your Name <you@example.com>
2024-12-19 23:54:15 +0800

    1

---
+++ 52fec55d-d54f-459c-b502-0bfc51493601.ts
@@ -0,0 +1,45 @@
+export default {
+  "entity": {
+    "id": "52fec55d-d54f-459c-b502-0bfc51493601",
+    "name": "反修 1967.9.14",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/52fec55d-d54f-459c-b502-0bfc51493601/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/52fec55d-d54f-459c-b502-0bfc51493601/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/52fec55d-d54f-459c-b502-0bfc51493601/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/52fec55d-d54f-459c-b502-0bfc51493601/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/52fec55d-d54f-459c-b502-0bfc51493601/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/52fec55d-d54f-459c-b502-0bfc51493601/6.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】反修 1967.9.14",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 6,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 14
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "52fec55d-d54f-459c-b502-0bfc51493601",
+  "resource_type": "book",
+  "version": 2
+}

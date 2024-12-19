a1d00c44d73eba6f99e4b4db88715654d508c45e
Your Name <you@example.com>
2024-12-20 00:11:15 +0800

    1

---
+++ 68c05d08-2c8f-4f82-96ea-c3297e95839a.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "68c05d08-2c8f-4f82-96ea-c3297e95839a",
+    "name": "工交战报 1967.6.16",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/68c05d08-2c8f-4f82-96ea-c3297e95839a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/68c05d08-2c8f-4f82-96ea-c3297e95839a/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工交战报 1967.6.16",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 6,
+            "day": 16
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "68c05d08-2c8f-4f82-96ea-c3297e95839a",
+  "resource_type": "book",
+  "version": 2
+}

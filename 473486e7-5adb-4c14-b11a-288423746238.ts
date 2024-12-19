3c4deb416c762373f432911d06901760b48cae3f
Your Name <you@example.com>
2024-12-20 01:04:24 +0800

    1

---
+++ 473486e7-5adb-4c14-b11a-288423746238.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "473486e7-5adb-4c14-b11a-288423746238",
+    "name": "看今朝 1967.9.22",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/473486e7-5adb-4c14-b11a-288423746238/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/473486e7-5adb-4c14-b11a-288423746238/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/473486e7-5adb-4c14-b11a-288423746238/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】看今朝 1967.9.22",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 22
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "473486e7-5adb-4c14-b11a-288423746238",
+  "resource_type": "book",
+  "version": 2
+}

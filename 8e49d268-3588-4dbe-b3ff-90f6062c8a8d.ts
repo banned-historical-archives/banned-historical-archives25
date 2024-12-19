0d702e7433abdd2420106f11758471698f56d894
Your Name <you@example.com>
2024-12-20 00:34:12 +0800

    1

---
+++ 8e49d268-3588-4dbe-b3ff-90f6062c8a8d.ts
@@ -0,0 +1,45 @@
+export default {
+  "entity": {
+    "id": "8e49d268-3588-4dbe-b3ff-90f6062c8a8d",
+    "name": "文艺 1967.10.28",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e49d268-3588-4dbe-b3ff-90f6062c8a8d/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e49d268-3588-4dbe-b3ff-90f6062c8a8d/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e49d268-3588-4dbe-b3ff-90f6062c8a8d/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e49d268-3588-4dbe-b3ff-90f6062c8a8d/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e49d268-3588-4dbe-b3ff-90f6062c8a8d/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8e49d268-3588-4dbe-b3ff-90f6062c8a8d/6.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】文艺 1967.10.28",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 6,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 10,
+            "day": 28
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "8e49d268-3588-4dbe-b3ff-90f6062c8a8d",
+  "resource_type": "book",
+  "version": 2
+}

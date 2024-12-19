7e2f307ea9dabcd26b312f2bbad38948294600c6
Your Name <you@example.com>
2024-12-20 00:54:39 +0800

    1

---
+++ 4f6ddb13-f49b-4c1e-9f12-61cbaa10c94a.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "4f6ddb13-f49b-4c1e-9f12-61cbaa10c94a",
+    "name": "无锡工人 1969.4.15",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4f6ddb13-f49b-4c1e-9f12-61cbaa10c94a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4f6ddb13-f49b-4c1e-9f12-61cbaa10c94a/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4f6ddb13-f49b-4c1e-9f12-61cbaa10c94a/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4f6ddb13-f49b-4c1e-9f12-61cbaa10c94a/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】无锡工人 1969.4.15",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 4,
+            "day": 15
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "4f6ddb13-f49b-4c1e-9f12-61cbaa10c94a",
+  "resource_type": "book",
+  "version": 2
+}

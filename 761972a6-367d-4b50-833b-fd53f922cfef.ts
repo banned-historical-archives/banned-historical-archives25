946d50a4aacb82e2db8a4f91a5bd7c1032c276f1
Your Name <you@example.com>
2024-12-20 00:15:18 +0800

    1

---
+++ 761972a6-367d-4b50-833b-fd53f922cfef.ts
@@ -0,0 +1,45 @@
+export default {
+  "entity": {
+    "id": "761972a6-367d-4b50-833b-fd53f922cfef",
+    "name": "工人造反 1969.3.5",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/761972a6-367d-4b50-833b-fd53f922cfef/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/761972a6-367d-4b50-833b-fd53f922cfef/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/761972a6-367d-4b50-833b-fd53f922cfef/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/761972a6-367d-4b50-833b-fd53f922cfef/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/761972a6-367d-4b50-833b-fd53f922cfef/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/761972a6-367d-4b50-833b-fd53f922cfef/6.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工人造反 1969.3.5",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 6,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 3,
+            "day": 5
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "761972a6-367d-4b50-833b-fd53f922cfef",
+  "resource_type": "book",
+  "version": 2
+}

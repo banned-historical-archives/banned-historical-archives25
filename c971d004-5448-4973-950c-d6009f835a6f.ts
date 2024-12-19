0d1e2d59eb0d30c4650f7cdee7afe26cd0bd39ff
Your Name <you@example.com>
2024-12-20 02:15:33 +0800

    1

---
+++ c971d004-5448-4973-950c-d6009f835a6f.ts
@@ -0,0 +1,44 @@
+export default {
+  "entity": {
+    "id": "c971d004-5448-4973-950c-d6009f835a6f",
+    "name": "飞鸣镝 1967.5.20",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c971d004-5448-4973-950c-d6009f835a6f/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c971d004-5448-4973-950c-d6009f835a6f/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c971d004-5448-4973-950c-d6009f835a6f/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c971d004-5448-4973-950c-d6009f835a6f/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c971d004-5448-4973-950c-d6009f835a6f/5.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】飞鸣镝 1967.5.20",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 5,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 5,
+            "day": 20
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "c971d004-5448-4973-950c-d6009f835a6f",
+  "resource_type": "book",
+  "version": 2
+}

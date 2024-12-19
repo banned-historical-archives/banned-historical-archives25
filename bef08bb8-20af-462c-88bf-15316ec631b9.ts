085a755e9bb119de0e00e4225cf0b3e0d01e4a18
Your Name <you@example.com>
2024-12-19 23:27:15 +0800

    1

---
+++ bef08bb8-20af-462c-88bf-15316ec631b9.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "bef08bb8-20af-462c-88bf-15316ec631b9",
+    "name": "五建革造81战报 1967.10.5",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/bef08bb8-20af-462c-88bf-15316ec631b9/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/bef08bb8-20af-462c-88bf-15316ec631b9/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】五建革造81战报 1967.10.5",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 10,
+            "day": 5
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "bef08bb8-20af-462c-88bf-15316ec631b9",
+  "resource_type": "book",
+  "version": 2
+}

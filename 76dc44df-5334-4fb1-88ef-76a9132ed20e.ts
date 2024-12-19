ae766779d43bdbd8aedd3b9dcb9bdbe3c7b7b700
Your Name <you@example.com>
2024-12-20 00:57:00 +0800

    1

---
+++ 76dc44df-5334-4fb1-88ef-76a9132ed20e.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "76dc44df-5334-4fb1-88ef-76a9132ed20e",
+    "name": "武汉钢二司 1967.8.20",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/76dc44df-5334-4fb1-88ef-76a9132ed20e/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/76dc44df-5334-4fb1-88ef-76a9132ed20e/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】武汉钢二司 1967.8.20",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 20
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "76dc44df-5334-4fb1-88ef-76a9132ed20e",
+  "resource_type": "book",
+  "version": 2
+}

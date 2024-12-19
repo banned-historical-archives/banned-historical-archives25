9693e8dfbb71f18fc41251befc0dfc7ab07fbbdc
Your Name <you@example.com>
2024-12-20 01:02:29 +0800

    1

---
+++ 341f70dc-a25e-442d-b37d-c5eb77df0d22.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "341f70dc-a25e-442d-b37d-c5eb77df0d22",
+    "name": "浙江工人 1968.9.20",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/341f70dc-a25e-442d-b37d-c5eb77df0d22/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/341f70dc-a25e-442d-b37d-c5eb77df0d22/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/341f70dc-a25e-442d-b37d-c5eb77df0d22/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/341f70dc-a25e-442d-b37d-c5eb77df0d22/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】浙江工人 1968.9.20",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 9,
+            "day": 20
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "341f70dc-a25e-442d-b37d-c5eb77df0d22",
+  "resource_type": "book",
+  "version": 2
+}

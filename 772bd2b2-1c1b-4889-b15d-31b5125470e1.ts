d1dabd1ef660561016c838db37195dd001f3590d
Your Name <you@example.com>
2024-12-19 23:26:58 +0800

    1

---
+++ 772bd2b2-1c1b-4889-b15d-31b5125470e1.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "772bd2b2-1c1b-4889-b15d-31b5125470e1",
+    "name": "五四风雷 1968.1.24",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/772bd2b2-1c1b-4889-b15d-31b5125470e1/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/772bd2b2-1c1b-4889-b15d-31b5125470e1/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】五四风雷 1968.1.24",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 1,
+            "day": 24
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "772bd2b2-1c1b-4889-b15d-31b5125470e1",
+  "resource_type": "book",
+  "version": 2
+}

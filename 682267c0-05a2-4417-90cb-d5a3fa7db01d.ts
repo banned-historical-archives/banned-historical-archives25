999a57edfd3f307f7e455b1543dfa8517dd07162
Your Name <you@example.com>
2024-12-19 23:59:19 +0800

    1

---
+++ 682267c0-05a2-4417-90cb-d5a3fa7db01d.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "682267c0-05a2-4417-90cb-d5a3fa7db01d",
+    "name": "在险峰 1967.8.27",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/682267c0-05a2-4417-90cb-d5a3fa7db01d/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/682267c0-05a2-4417-90cb-d5a3fa7db01d/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】在险峰 1967.8.27",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 27
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "682267c0-05a2-4417-90cb-d5a3fa7db01d",
+  "resource_type": "book",
+  "version": 2
+}

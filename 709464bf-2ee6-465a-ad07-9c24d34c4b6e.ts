a1b4d1589bd98679c785912a2829427d4b775a93
Your Name <you@example.com>
2024-12-20 02:17:58 +0800

    1

---
+++ 709464bf-2ee6-465a-ad07-9c24d34c4b6e.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "709464bf-2ee6-465a-ad07-9c24d34c4b6e",
+    "name": "首都红卫兵 1967.3.21",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/709464bf-2ee6-465a-ad07-9c24d34c4b6e/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/709464bf-2ee6-465a-ad07-9c24d34c4b6e/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/709464bf-2ee6-465a-ad07-9c24d34c4b6e/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/709464bf-2ee6-465a-ad07-9c24d34c4b6e/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】首都红卫兵 1967.3.21",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 3,
+            "day": 21
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "709464bf-2ee6-465a-ad07-9c24d34c4b6e",
+  "resource_type": "book",
+  "version": 2
+}

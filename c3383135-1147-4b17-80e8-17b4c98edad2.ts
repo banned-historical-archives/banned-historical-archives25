6f236a2ae9d5fbbe3a7b00315e8c88d5588d1a91
Your Name <you@example.com>
2024-12-19 23:53:43 +0800

    1

---
+++ c3383135-1147-4b17-80e8-17b4c98edad2.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "c3383135-1147-4b17-80e8-17b4c98edad2",
+    "name": "参政消息 1967.9.3",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c3383135-1147-4b17-80e8-17b4c98edad2/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c3383135-1147-4b17-80e8-17b4c98edad2/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】参政消息 1967.9.3",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 3
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "c3383135-1147-4b17-80e8-17b4c98edad2",
+  "resource_type": "book",
+  "version": 2
+}

818a05423eb2f11e7012d2a27bd38b82b11de028
Your Name <you@example.com>
2024-12-19 23:25:12 +0800

    1

---
+++ 98f068e2-5a1d-4d97-824b-0bd24c43868b.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "98f068e2-5a1d-4d97-824b-0bd24c43868b",
+    "name": "东方红 1967.8.4",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/98f068e2-5a1d-4d97-824b-0bd24c43868b/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/98f068e2-5a1d-4d97-824b-0bd24c43868b/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/98f068e2-5a1d-4d97-824b-0bd24c43868b/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/98f068e2-5a1d-4d97-824b-0bd24c43868b/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】东方红 1967.8.4",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 4
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "98f068e2-5a1d-4d97-824b-0bd24c43868b",
+  "resource_type": "book",
+  "version": 2
+}

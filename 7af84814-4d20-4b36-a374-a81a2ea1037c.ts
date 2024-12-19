cd2d8fe20189089446b5ff30c4d8b38b5afe38d8
Your Name <you@example.com>
2024-12-20 00:25:52 +0800

    1

---
+++ 7af84814-4d20-4b36-a374-a81a2ea1037c.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "7af84814-4d20-4b36-a374-a81a2ea1037c",
+    "name": "惊雷 1967.9.2",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/7af84814-4d20-4b36-a374-a81a2ea1037c/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/7af84814-4d20-4b36-a374-a81a2ea1037c/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/7af84814-4d20-4b36-a374-a81a2ea1037c/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】惊雷 1967.9.2",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 2
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "7af84814-4d20-4b36-a374-a81a2ea1037c",
+  "resource_type": "book",
+  "version": 2
+}

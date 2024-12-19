559468a5666ae6fb9201db4810a9209b30add0d1
Your Name <you@example.com>
2024-12-20 00:47:16 +0800

    1

---
+++ c9525e79-c863-4908-b375-22f7a2435ebb.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "c9525e79-c863-4908-b375-22f7a2435ebb",
+    "name": "文革通讯 1967.8.28",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c9525e79-c863-4908-b375-22f7a2435ebb/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c9525e79-c863-4908-b375-22f7a2435ebb/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c9525e79-c863-4908-b375-22f7a2435ebb/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】文革通讯 1967.8.28",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 28
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "c9525e79-c863-4908-b375-22f7a2435ebb",
+  "resource_type": "book",
+  "version": 2
+}

9f0e9f0963ef4e045275dc38063ed91ee17df134
Your Name <you@example.com>
2024-12-20 00:58:57 +0800

    1

---
+++ 4e87fea9-c305-4f7d-92aa-44b110666d8e.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "4e87fea9-c305-4f7d-92aa-44b110666d8e",
+    "name": "江苏红卫兵 1968.4.10",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4e87fea9-c305-4f7d-92aa-44b110666d8e/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4e87fea9-c305-4f7d-92aa-44b110666d8e/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4e87fea9-c305-4f7d-92aa-44b110666d8e/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4e87fea9-c305-4f7d-92aa-44b110666d8e/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】江苏红卫兵 1968.4.10",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 4,
+            "day": 10
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "4e87fea9-c305-4f7d-92aa-44b110666d8e",
+  "resource_type": "book",
+  "version": 2
+}

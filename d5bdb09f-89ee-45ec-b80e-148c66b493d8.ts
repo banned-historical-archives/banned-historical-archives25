0d5f8ee66fbb7a11e26722898696dab796155819
Your Name <you@example.com>
2024-12-20 00:05:40 +0800

    1

---
+++ d5bdb09f-89ee-45ec-b80e-148c66b493d8.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "d5bdb09f-89ee-45ec-b80e-148c66b493d8",
+    "name": "展宏图 1975.12.1",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/d5bdb09f-89ee-45ec-b80e-148c66b493d8/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】展宏图 1975.12.1",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1975,
+            "month": 12,
+            "day": 1
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "d5bdb09f-89ee-45ec-b80e-148c66b493d8",
+  "resource_type": "book",
+  "version": 2
+}

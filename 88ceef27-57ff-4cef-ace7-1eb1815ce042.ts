3bfeb70b198eff105ee86027da00a0a87183617f
Your Name <you@example.com>
2024-12-19 23:48:41 +0800

    1

---
+++ 88ceef27-57ff-4cef-ace7-1eb1815ce042.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "88ceef27-57ff-4cef-ace7-1eb1815ce042",
+    "name": "千钧棒 1967.9.11",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/88ceef27-57ff-4cef-ace7-1eb1815ce042/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/88ceef27-57ff-4cef-ace7-1eb1815ce042/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/88ceef27-57ff-4cef-ace7-1eb1815ce042/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/88ceef27-57ff-4cef-ace7-1eb1815ce042/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】千钧棒 1967.9.11",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 11
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "88ceef27-57ff-4cef-ace7-1eb1815ce042",
+  "resource_type": "book",
+  "version": 2
+}

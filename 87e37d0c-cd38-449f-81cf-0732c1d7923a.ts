643607fdc07b171b9356b2d3575001dd006ba5db
Your Name <you@example.com>
2024-12-20 00:29:54 +0800

    1

---
+++ 87e37d0c-cd38-449f-81cf-0732c1d7923a.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "87e37d0c-cd38-449f-81cf-0732c1d7923a",
+    "name": "批斗动态 1967.9.7",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/87e37d0c-cd38-449f-81cf-0732c1d7923a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/87e37d0c-cd38-449f-81cf-0732c1d7923a/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】批斗动态 1967.9.7",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 7
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "87e37d0c-cd38-449f-81cf-0732c1d7923a",
+  "resource_type": "book",
+  "version": 2
+}

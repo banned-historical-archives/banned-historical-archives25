905f83441e4dd5b508b5b98668ab5c22a4401617
Your Name <you@example.com>
2024-12-20 02:10:12 +0800

    1

---
+++ 6812474d-1f3d-442c-a6af-99614c333c62.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "6812474d-1f3d-442c-a6af-99614c333c62",
+    "name": "进军报 1967.5.6",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6812474d-1f3d-442c-a6af-99614c333c62/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6812474d-1f3d-442c-a6af-99614c333c62/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6812474d-1f3d-442c-a6af-99614c333c62/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6812474d-1f3d-442c-a6af-99614c333c62/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】进军报 1967.5.6",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 5,
+            "day": 6
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "6812474d-1f3d-442c-a6af-99614c333c62",
+  "resource_type": "book",
+  "version": 2
+}

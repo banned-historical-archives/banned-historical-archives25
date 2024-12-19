4fc3fe8cba91e488181897be08a74aea8430cc69
Your Name <you@example.com>
2024-12-19 23:31:33 +0800

    1

---
+++ 146ae554-0ef3-4d37-9014-8b6c5d8170c7.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "146ae554-0ef3-4d37-9014-8b6c5d8170c7",
+    "name": "井冈山 1969.7.1",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/146ae554-0ef3-4d37-9014-8b6c5d8170c7/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/146ae554-0ef3-4d37-9014-8b6c5d8170c7/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/146ae554-0ef3-4d37-9014-8b6c5d8170c7/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/146ae554-0ef3-4d37-9014-8b6c5d8170c7/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】井冈山 1969.7.1",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 7,
+            "day": 1
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "146ae554-0ef3-4d37-9014-8b6c5d8170c7",
+  "resource_type": "book",
+  "version": 2
+}

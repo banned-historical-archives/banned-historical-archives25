fcf31845b50f2f7ad004e59de0fb11469bee5fd0
Your Name <you@example.com>
2024-12-20 02:06:08 +0800

    1

---
+++ 493ab039-e751-4e46-8b22-9d49e80f6234.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "493ab039-e751-4e46-8b22-9d49e80f6234",
+    "name": "绍兴通讯 1968.10.11",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/493ab039-e751-4e46-8b22-9d49e80f6234/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/493ab039-e751-4e46-8b22-9d49e80f6234/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/493ab039-e751-4e46-8b22-9d49e80f6234/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/493ab039-e751-4e46-8b22-9d49e80f6234/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】绍兴通讯 1968.10.11",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 10,
+            "day": 11
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "493ab039-e751-4e46-8b22-9d49e80f6234",
+  "resource_type": "book",
+  "version": 2
+}

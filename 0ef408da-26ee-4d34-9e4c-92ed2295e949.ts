8975a6abaada2f91eb181b1aa4408174662c7035
Your Name <you@example.com>
2024-12-20 01:46:55 +0800

    1

---
+++ 0ef408da-26ee-4d34-9e4c-92ed2295e949.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "0ef408da-26ee-4d34-9e4c-92ed2295e949",
+    "name": "红色风暴 1969.2.6",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ef408da-26ee-4d34-9e4c-92ed2295e949/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ef408da-26ee-4d34-9e4c-92ed2295e949/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ef408da-26ee-4d34-9e4c-92ed2295e949/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/0ef408da-26ee-4d34-9e4c-92ed2295e949/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】红色风暴 1969.2.6",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 2,
+            "day": 6
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "0ef408da-26ee-4d34-9e4c-92ed2295e949",
+  "resource_type": "book",
+  "version": 2
+}

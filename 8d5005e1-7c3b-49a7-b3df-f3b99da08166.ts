caa220a78d0a744e64d947a8377c8e286952d020
Your Name <you@example.com>
2024-12-20 01:03:57 +0800

    1

---
+++ 8d5005e1-7c3b-49a7-b3df-f3b99da08166.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "8d5005e1-7c3b-49a7-b3df-f3b99da08166",
+    "name": "瓷都红卫兵 1967.1.24",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8d5005e1-7c3b-49a7-b3df-f3b99da08166/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8d5005e1-7c3b-49a7-b3df-f3b99da08166/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8d5005e1-7c3b-49a7-b3df-f3b99da08166/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8d5005e1-7c3b-49a7-b3df-f3b99da08166/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】瓷都红卫兵 1967.1.24",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 1,
+            "day": 24
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "8d5005e1-7c3b-49a7-b3df-f3b99da08166",
+  "resource_type": "book",
+  "version": 2
+}

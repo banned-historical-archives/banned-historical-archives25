eb22b28f2c00497311a9a9033c057a809ad4ca8a
Your Name <you@example.com>
2024-12-20 01:06:11 +0800

    1

---
+++ 836720d2-4025-4f3c-a477-81564eff053f.ts
@@ -0,0 +1,46 @@
+export default {
+  "entity": {
+    "id": "836720d2-4025-4f3c-a477-81564eff053f",
+    "name": "红卫兵战地 1967.8.6",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/836720d2-4025-4f3c-a477-81564eff053f/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/836720d2-4025-4f3c-a477-81564eff053f/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/836720d2-4025-4f3c-a477-81564eff053f/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/836720d2-4025-4f3c-a477-81564eff053f/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/836720d2-4025-4f3c-a477-81564eff053f/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/836720d2-4025-4f3c-a477-81564eff053f/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/836720d2-4025-4f3c-a477-81564eff053f/7.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】红卫兵战地 1967.8.6",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 7,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 6
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "836720d2-4025-4f3c-a477-81564eff053f",
+  "resource_type": "book",
+  "version": 2
+}

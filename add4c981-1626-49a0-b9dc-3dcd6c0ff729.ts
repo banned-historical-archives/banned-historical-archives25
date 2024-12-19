d0b813d3217ecf08f09cfca8d7da79f7ef21dd3e
Your Name <you@example.com>
2024-12-20 00:51:01 +0800

    1

---
+++ add4c981-1626-49a0-b9dc-3dcd6c0ff729.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "add4c981-1626-49a0-b9dc-3dcd6c0ff729",
+    "name": "新人大 1967.6.22",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/add4c981-1626-49a0-b9dc-3dcd6c0ff729/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/add4c981-1626-49a0-b9dc-3dcd6c0ff729/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/add4c981-1626-49a0-b9dc-3dcd6c0ff729/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/add4c981-1626-49a0-b9dc-3dcd6c0ff729/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】新人大 1967.6.22",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 6,
+            "day": 22
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "add4c981-1626-49a0-b9dc-3dcd6c0ff729",
+  "resource_type": "book",
+  "version": 2
+}

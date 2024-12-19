b7d57a5dfc774a33a0ad141b44815260d2185b68
Your Name <you@example.com>
2024-12-20 00:49:00 +0800

    1

---
+++ 85a530f3-f22a-4870-830c-c61bc9daad23.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "85a530f3-f22a-4870-830c-c61bc9daad23",
+    "name": "文革风雷 1967.9.17",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/85a530f3-f22a-4870-830c-c61bc9daad23/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/85a530f3-f22a-4870-830c-c61bc9daad23/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/85a530f3-f22a-4870-830c-c61bc9daad23/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】文革风雷 1967.9.17",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 17
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "85a530f3-f22a-4870-830c-c61bc9daad23",
+  "resource_type": "book",
+  "version": 2
+}

4cbf5936397b13a656291e54379645155d2688ee
Your Name <you@example.com>
2024-12-20 00:52:34 +0800

    1

---
+++ 047bc164-5719-4ab4-8ff3-22290844cff8.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "047bc164-5719-4ab4-8ff3-22290844cff8",
+    "name": "新北大 1967.1.31",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/047bc164-5719-4ab4-8ff3-22290844cff8/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/047bc164-5719-4ab4-8ff3-22290844cff8/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/047bc164-5719-4ab4-8ff3-22290844cff8/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/047bc164-5719-4ab4-8ff3-22290844cff8/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】新北大 1967.1.31",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 1,
+            "day": 31
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "047bc164-5719-4ab4-8ff3-22290844cff8",
+  "resource_type": "book",
+  "version": 2
+}

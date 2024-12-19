991b7f60910ed38f87098d3eb610f76e684094ab
Your Name <you@example.com>
2024-12-20 00:03:42 +0800

    1

---
+++ a5fb275a-3c48-4b38-9c86-392abe8fdeb2.ts
@@ -0,0 +1,44 @@
+export default {
+  "entity": {
+    "id": "a5fb275a-3c48-4b38-9c86-392abe8fdeb2",
+    "name": "大批判 1967.9.29",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a5fb275a-3c48-4b38-9c86-392abe8fdeb2/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a5fb275a-3c48-4b38-9c86-392abe8fdeb2/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a5fb275a-3c48-4b38-9c86-392abe8fdeb2/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a5fb275a-3c48-4b38-9c86-392abe8fdeb2/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/a5fb275a-3c48-4b38-9c86-392abe8fdeb2/5.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】大批判 1967.9.29",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 5,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 29
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "a5fb275a-3c48-4b38-9c86-392abe8fdeb2",
+  "resource_type": "book",
+  "version": 2
+}

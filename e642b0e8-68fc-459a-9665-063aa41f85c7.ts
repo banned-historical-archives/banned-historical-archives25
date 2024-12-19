f4e4681081cd0bdfabd6f3995eb5f30c7d0b7a43
Your Name <you@example.com>
2024-12-19 23:59:39 +0800

    1

---
+++ e642b0e8-68fc-459a-9665-063aa41f85c7.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "e642b0e8-68fc-459a-9665-063aa41f85c7",
+    "name": "地专战报 1967.11.6",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e642b0e8-68fc-459a-9665-063aa41f85c7/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e642b0e8-68fc-459a-9665-063aa41f85c7/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e642b0e8-68fc-459a-9665-063aa41f85c7/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e642b0e8-68fc-459a-9665-063aa41f85c7/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】地专战报 1967.11.6",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 11,
+            "day": 6
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "e642b0e8-68fc-459a-9665-063aa41f85c7",
+  "resource_type": "book",
+  "version": 2
+}

199d8cbe6a6e444c58478cd6fb953ec6d939ec20
Your Name <you@example.com>
2024-12-20 00:54:54 +0800

    1

---
+++ eabffff5-6b66-4301-ac3a-02a3a79a807b.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "eabffff5-6b66-4301-ac3a-02a3a79a807b",
+    "name": "春雷 1967.3.23",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/eabffff5-6b66-4301-ac3a-02a3a79a807b/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/eabffff5-6b66-4301-ac3a-02a3a79a807b/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】春雷 1967.3.23",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 3,
+            "day": 23
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "eabffff5-6b66-4301-ac3a-02a3a79a807b",
+  "resource_type": "book",
+  "version": 2
+}

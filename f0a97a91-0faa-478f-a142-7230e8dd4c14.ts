aa741c398391ff29fcba37af1226f5fd18fcdc7b
Your Name <you@example.com>
2024-12-19 23:37:17 +0800

    1

---
+++ f0a97a91-0faa-478f-a142-7230e8dd4c14.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "f0a97a91-0faa-478f-a142-7230e8dd4c14",
+    "name": "动态 1967.9.9",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/f0a97a91-0faa-478f-a142-7230e8dd4c14/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/f0a97a91-0faa-478f-a142-7230e8dd4c14/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/f0a97a91-0faa-478f-a142-7230e8dd4c14/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】动态 1967.9.9",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 9
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "f0a97a91-0faa-478f-a142-7230e8dd4c14",
+  "resource_type": "book",
+  "version": 2
+}

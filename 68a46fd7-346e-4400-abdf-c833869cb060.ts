86a78415c60fd987b0a2702ead4282d6eab53a1b
Your Name <you@example.com>
2024-12-20 00:10:57 +0800

    1

---
+++ 68a46fd7-346e-4400-abdf-c833869cb060.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "68a46fd7-346e-4400-abdf-c833869cb060",
+    "name": "工业系统大批判通讯 1967.9.10",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/68a46fd7-346e-4400-abdf-c833869cb060/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/68a46fd7-346e-4400-abdf-c833869cb060/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/68a46fd7-346e-4400-abdf-c833869cb060/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工业系统大批判通讯 1967.9.10",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 10
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "68a46fd7-346e-4400-abdf-c833869cb060",
+  "resource_type": "book",
+  "version": 2
+}

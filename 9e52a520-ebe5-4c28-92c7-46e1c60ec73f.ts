c65ac9a4d0d2be00be71ada67e5abc7b530d178d
Your Name <you@example.com>
2024-12-20 00:33:11 +0800

    1

---
+++ 9e52a520-ebe5-4c28-92c7-46e1c60ec73f.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "9e52a520-ebe5-4c28-92c7-46e1c60ec73f",
+    "name": "挺进快报 1967.2.8",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/9e52a520-ebe5-4c28-92c7-46e1c60ec73f/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/9e52a520-ebe5-4c28-92c7-46e1c60ec73f/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/9e52a520-ebe5-4c28-92c7-46e1c60ec73f/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】挺进快报 1967.2.8",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 2,
+            "day": 8
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "9e52a520-ebe5-4c28-92c7-46e1c60ec73f",
+  "resource_type": "book",
+  "version": 2
+}

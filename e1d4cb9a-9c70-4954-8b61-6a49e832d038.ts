519e527e5feabd0c739ea1f2b9aa4ea4346ae1bb
Your Name <you@example.com>
2024-12-20 00:10:04 +0800

    1

---
+++ e1d4cb9a-9c70-4954-8b61-6a49e832d038.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "e1d4cb9a-9c70-4954-8b61-6a49e832d038",
+    "name": "山东革命工人 1967.10.13",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1d4cb9a-9c70-4954-8b61-6a49e832d038/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/e1d4cb9a-9c70-4954-8b61-6a49e832d038/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】山东革命工人 1967.10.13",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 10,
+            "day": 13
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "e1d4cb9a-9c70-4954-8b61-6a49e832d038",
+  "resource_type": "book",
+  "version": 2
+}

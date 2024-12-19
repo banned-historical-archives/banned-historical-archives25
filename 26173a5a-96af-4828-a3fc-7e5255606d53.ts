97b0e46833489535162250ff9e3eab253887cf5f
Your Name <you@example.com>
2024-12-20 02:11:27 +0800

    1

---
+++ 26173a5a-96af-4828-a3fc-7e5255606d53.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "26173a5a-96af-4828-a3fc-7e5255606d53",
+    "name": "造反有理 1966.10.29",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/26173a5a-96af-4828-a3fc-7e5255606d53/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/26173a5a-96af-4828-a3fc-7e5255606d53/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】造反有理 1966.10.29",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1966,
+            "month": 10,
+            "day": 29
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "26173a5a-96af-4828-a3fc-7e5255606d53",
+  "resource_type": "book",
+  "version": 2
+}

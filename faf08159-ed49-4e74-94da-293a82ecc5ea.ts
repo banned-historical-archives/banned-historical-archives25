f09ab8a554f9474fdb32f2333dd84c280e0ee80f
Your Name <you@example.com>
2024-12-20 23:10:00 +0800

    1

---
+++ faf08159-ed49-4e74-94da-293a82ecc5ea.ts
@@ -0,0 +1,45 @@
+export default {
+  "entity": {
+    "id": "faf08159-ed49-4e74-94da-293a82ecc5ea",
+    "name": "北京消息 1967.9.7",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/faf08159-ed49-4e74-94da-293a82ecc5ea/1.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/faf08159-ed49-4e74-94da-293a82ecc5ea/2.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/faf08159-ed49-4e74-94da-293a82ecc5ea/3.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/faf08159-ed49-4e74-94da-293a82ecc5ea/4.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/faf08159-ed49-4e74-94da-293a82ecc5ea/5.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/faf08159-ed49-4e74-94da-293a82ecc5ea/6.png"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】北京消息 1967.9.7",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 6,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 7
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "faf08159-ed49-4e74-94da-293a82ecc5ea",
+  "resource_type": "book",
+  "version": 2
+}

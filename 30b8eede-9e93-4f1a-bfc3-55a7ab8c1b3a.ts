09e88d357e89013561e9569ceec0b207a1e4d5d5
Your Name <you@example.com>
2024-12-20 23:56:04 +0800

    1

---
+++ 30b8eede-9e93-4f1a-bfc3-55a7ab8c1b3a.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "30b8eede-9e93-4f1a-bfc3-55a7ab8c1b3a",
+    "name": "无产者通讯 1967.8.8",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/30b8eede-9e93-4f1a-bfc3-55a7ab8c1b3a/1.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/30b8eede-9e93-4f1a-bfc3-55a7ab8c1b3a/2.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/30b8eede-9e93-4f1a-bfc3-55a7ab8c1b3a/3.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/30b8eede-9e93-4f1a-bfc3-55a7ab8c1b3a/4.png"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】无产者通讯 1967.8.8",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 8
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "30b8eede-9e93-4f1a-bfc3-55a7ab8c1b3a",
+  "resource_type": "book",
+  "version": 2
+}

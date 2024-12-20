b23d9bdb98af60418aec5e1165622325661c4a5c
Your Name <you@example.com>
2024-12-20 23:15:12 +0800

    1

---
+++ 313390cc-0e7b-4458-91a7-7c0ba0d2455b.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "313390cc-0e7b-4458-91a7-7c0ba0d2455b",
+    "name": "河南二七报 1967.6.17",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/313390cc-0e7b-4458-91a7-7c0ba0d2455b/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/313390cc-0e7b-4458-91a7-7c0ba0d2455b/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】河南二七报 1967.6.17",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 6,
+            "day": 17
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "313390cc-0e7b-4458-91a7-7c0ba0d2455b",
+  "resource_type": "book",
+  "version": 2
+}

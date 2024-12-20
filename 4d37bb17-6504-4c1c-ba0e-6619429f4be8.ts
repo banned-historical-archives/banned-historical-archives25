f0fc14495aa27cb8bd6f5145897c91fe550e91b5
Your Name <you@example.com>
2024-12-20 23:09:35 +0800

    1

---
+++ 4d37bb17-6504-4c1c-ba0e-6619429f4be8.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "4d37bb17-6504-4c1c-ba0e-6619429f4be8",
+    "name": "动态报导 1967.4.19",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4d37bb17-6504-4c1c-ba0e-6619429f4be8/1.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4d37bb17-6504-4c1c-ba0e-6619429f4be8/2.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4d37bb17-6504-4c1c-ba0e-6619429f4be8/3.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/4d37bb17-6504-4c1c-ba0e-6619429f4be8/4.png"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】动态报导 1967.4.19",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 4,
+            "day": 19
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "4d37bb17-6504-4c1c-ba0e-6619429f4be8",
+  "resource_type": "book",
+  "version": 2
+}

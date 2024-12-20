3e07ffefb366f47b7f7284af6d959ab7ceefe22d
Your Name <you@example.com>
2024-12-21 00:04:59 +0800

    1

---
+++ 7378d9aa-056d-4c54-91b4-9d7d7de7789b.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "7378d9aa-056d-4c54-91b4-9d7d7de7789b",
+    "name": "外贸通讯 1967.8.15",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/7378d9aa-056d-4c54-91b4-9d7d7de7789b/1.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/7378d9aa-056d-4c54-91b4-9d7d7de7789b/2.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/7378d9aa-056d-4c54-91b4-9d7d7de7789b/3.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/7378d9aa-056d-4c54-91b4-9d7d7de7789b/4.png"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】外贸通讯 1967.8.15",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 15
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "7378d9aa-056d-4c54-91b4-9d7d7de7789b",
+  "resource_type": "book",
+  "version": 2
+}

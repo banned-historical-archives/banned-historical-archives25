32ceef3a3c201c03ce4628c0145637f551380d8e
Your Name <you@example.com>
2024-12-20 23:16:00 +0800

    1

---
+++ 6fecf4a2-da8b-4273-9dbe-8e69b207bf2b.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "6fecf4a2-da8b-4273-9dbe-8e69b207bf2b",
+    "name": "红旗 1967.7.12",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6fecf4a2-da8b-4273-9dbe-8e69b207bf2b/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】红旗 1967.7.12",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 12
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "6fecf4a2-da8b-4273-9dbe-8e69b207bf2b",
+  "resource_type": "book",
+  "version": 2
+}

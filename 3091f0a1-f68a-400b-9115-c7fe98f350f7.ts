0040f6ae79370fc780db1a6b7c624cd6547e9e2b
Your Name <you@example.com>
2024-12-21 00:02:51 +0800

    1

---
+++ 3091f0a1-f68a-400b-9115-c7fe98f350f7.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "3091f0a1-f68a-400b-9115-c7fe98f350f7",
+    "name": "跃进快报 1958.5.28",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/3091f0a1-f68a-400b-9115-c7fe98f350f7/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】跃进快报 1958.5.28",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1958,
+            "month": 5,
+            "day": 28
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "3091f0a1-f68a-400b-9115-c7fe98f350f7",
+  "resource_type": "book",
+  "version": 2
+}

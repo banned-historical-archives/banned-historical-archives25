4da3b6c0690270a51d55543f22546a6305c92590
Your Name <you@example.com>
2024-12-20 23:54:33 +0800

    1

---
+++ ae039506-3be9-4033-b9f8-37a6f53c39df.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "ae039506-3be9-4033-b9f8-37a6f53c39df",
+    "name": "战报 1967.10.15",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/ae039506-3be9-4033-b9f8-37a6f53c39df/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】战报 1967.10.15",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 10,
+            "day": 15
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "ae039506-3be9-4033-b9f8-37a6f53c39df",
+  "resource_type": "book",
+  "version": 2
+}

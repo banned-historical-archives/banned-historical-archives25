1b8609d51689cae59d672ed5ab2601d3a3ebace9
Your Name <you@example.com>
2024-12-21 00:03:04 +0800

    1

---
+++ dcef918a-7c3f-48ed-89b3-9d978dd16b06.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "dcef918a-7c3f-48ed-89b3-9d978dd16b06",
+    "name": "跃进战报 1971.12.14",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/dcef918a-7c3f-48ed-89b3-9d978dd16b06/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】跃进战报 1971.12.14",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1971,
+            "month": 12,
+            "day": 14
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "dcef918a-7c3f-48ed-89b3-9d978dd16b06",
+  "resource_type": "book",
+  "version": 2
+}

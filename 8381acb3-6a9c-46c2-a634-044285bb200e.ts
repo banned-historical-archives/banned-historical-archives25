87640599b738bb7a80993894d6d9d347c787429d
Your Name <you@example.com>
2024-12-20 23:53:46 +0800

    1

---
+++ 8381acb3-6a9c-46c2-a634-044285bb200e.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "8381acb3-6a9c-46c2-a634-044285bb200e",
+    "name": "展宏图 1975.11.22",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/8381acb3-6a9c-46c2-a634-044285bb200e/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】展宏图 1975.11.22",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1975,
+            "month": 11,
+            "day": 22
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "8381acb3-6a9c-46c2-a634-044285bb200e",
+  "resource_type": "book",
+  "version": 2
+}

32bd04063b18e3fd560af5d498ab475eddf7fe4f
Your Name <you@example.com>
2024-12-20 23:56:19 +0800

    1

---
+++ 941f6576-f367-4f92-8047-ce100a06b2c0.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "941f6576-f367-4f92-8047-ce100a06b2c0",
+    "name": "武汉烈火 1967.7.31",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/941f6576-f367-4f92-8047-ce100a06b2c0/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/941f6576-f367-4f92-8047-ce100a06b2c0/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】武汉烈火 1967.7.31",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 31
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "941f6576-f367-4f92-8047-ce100a06b2c0",
+  "resource_type": "book",
+  "version": 2
+}

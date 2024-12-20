567db96155e841af9592bb1e4fda581bf9916a53
Your Name <you@example.com>
2024-12-20 23:14:09 +0800

    1

---
+++ 6b92de37-868c-4413-876d-913f2c58c3ee.ts
@@ -0,0 +1,52 @@
+export default {
+  "entity": {
+    "id": "6b92de37-868c-4413-876d-913f2c58c3ee",
+    "name": "工人造反报 1969.11.20",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6b92de37-868c-4413-876d-913f2c58c3ee/13.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工人造反报 1969.11.20",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 13,
+        "dates": [
+          {
+            "year": 1969,
+            "month": 11,
+            "day": 20
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "6b92de37-868c-4413-876d-913f2c58c3ee",
+  "resource_type": "book",
+  "version": 2
+}

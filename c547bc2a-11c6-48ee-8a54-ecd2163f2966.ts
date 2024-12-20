6a0d48837523f34b976fa04cbab1b2c1fc1ab77f
Your Name <you@example.com>
2024-12-20 23:10:45 +0800

    1

---
+++ c547bc2a-11c6-48ee-8a54-ecd2163f2966.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "c547bc2a-11c6-48ee-8a54-ecd2163f2966",
+    "name": "北斗星 1967.9.10",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c547bc2a-11c6-48ee-8a54-ecd2163f2966/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c547bc2a-11c6-48ee-8a54-ecd2163f2966/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c547bc2a-11c6-48ee-8a54-ecd2163f2966/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】北斗星 1967.9.10",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 10
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "c547bc2a-11c6-48ee-8a54-ecd2163f2966",
+  "resource_type": "book",
+  "version": 2
+}

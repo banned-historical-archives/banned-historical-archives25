f5199dcca7a7b2d2c619a814fdac2583abfaa56e
Your Name <you@example.com>
2024-12-20 23:56:35 +0800

    1

---
+++ 816b2c41-8c8f-474f-ac2f-a578c8a956f8.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "816b2c41-8c8f-474f-ac2f-a578c8a956f8",
+    "name": "长春红卫兵 1967.10.13",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/816b2c41-8c8f-474f-ac2f-a578c8a956f8/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/816b2c41-8c8f-474f-ac2f-a578c8a956f8/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】长春红卫兵 1967.10.13",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 10,
+            "day": 13
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "816b2c41-8c8f-474f-ac2f-a578c8a956f8",
+  "resource_type": "book",
+  "version": 2
+}

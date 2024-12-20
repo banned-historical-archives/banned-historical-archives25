916b73f0b383736c380c97bca7dd2a29829828bb
Your Name <you@example.com>
2024-12-20 23:16:44 +0800

    1

---
+++ 5652d716-74be-401d-8c0b-2afb97a55cdf.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "5652d716-74be-401d-8c0b-2afb97a55cdf",
+    "name": "红色工人 1967.11.2",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/5652d716-74be-401d-8c0b-2afb97a55cdf/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】红色工人 1967.11.2",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 11,
+            "day": 2
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "5652d716-74be-401d-8c0b-2afb97a55cdf",
+  "resource_type": "book",
+  "version": 2
+}

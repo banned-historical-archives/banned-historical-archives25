bb73b3810cec48e0f15e4f81bf4c65efaeeef5bc
Your Name <you@example.com>
2024-12-20 23:08:32 +0800

    1

---
+++ cb5e9613-1d27-48e8-9d36-53fb66d58e6a.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "cb5e9613-1d27-48e8-9d36-53fb66d58e6a",
+    "name": "八八战报 1967.7.25",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/cb5e9613-1d27-48e8-9d36-53fb66d58e6a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/cb5e9613-1d27-48e8-9d36-53fb66d58e6a/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/cb5e9613-1d27-48e8-9d36-53fb66d58e6a/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/cb5e9613-1d27-48e8-9d36-53fb66d58e6a/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】八八战报 1967.7.25",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 7,
+            "day": 25
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "cb5e9613-1d27-48e8-9d36-53fb66d58e6a",
+  "resource_type": "book",
+  "version": 2
+}

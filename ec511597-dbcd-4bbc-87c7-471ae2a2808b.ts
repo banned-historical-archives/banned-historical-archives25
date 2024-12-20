b6b4fe812984ecf53f5ce7f43d90b0b70343677c
Your Name <you@example.com>
2024-12-20 23:12:17 +0800

    1

---
+++ ec511597-dbcd-4bbc-87c7-471ae2a2808b.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "ec511597-dbcd-4bbc-87c7-471ae2a2808b",
+    "name": "大修战报 1978.12.21",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/ec511597-dbcd-4bbc-87c7-471ae2a2808b/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】大修战报 1978.12.21",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1978,
+            "month": 12,
+            "day": 21
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "ec511597-dbcd-4bbc-87c7-471ae2a2808b",
+  "resource_type": "book",
+  "version": 2
+}

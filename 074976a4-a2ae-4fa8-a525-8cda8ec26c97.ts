2dce2b418df613e4c7e51ab6095527ee474971b5
Your Name <you@example.com>
2024-12-20 23:53:15 +0800

    1

---
+++ 074976a4-a2ae-4fa8-a525-8cda8ec26c97.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "074976a4-a2ae-4fa8-a525-8cda8ec26c97",
+    "name": "五洲风雷 1967.8.5",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/074976a4-a2ae-4fa8-a525-8cda8ec26c97/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/074976a4-a2ae-4fa8-a525-8cda8ec26c97/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】五洲风雷 1967.8.5",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 5
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "074976a4-a2ae-4fa8-a525-8cda8ec26c97",
+  "resource_type": "book",
+  "version": 2
+}

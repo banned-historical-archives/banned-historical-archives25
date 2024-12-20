9c7b699dbd318074270f133982d08e1afe65c5b2
Your Name <you@example.com>
2024-12-20 23:17:48 +0800

    1

---
+++ 6852d23e-88cf-40a4-a5fc-3067573d2d69.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "6852d23e-88cf-40a4-a5fc-3067573d2d69",
+    "name": "飞鸣镝 1967.8.25",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6852d23e-88cf-40a4-a5fc-3067573d2d69/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/6852d23e-88cf-40a4-a5fc-3067573d2d69/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】飞鸣镝 1967.8.25",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 25
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "6852d23e-88cf-40a4-a5fc-3067573d2d69",
+  "resource_type": "book",
+  "version": 2
+}

fdb0eb610bf0900293be2e558731d5a14222aae6
Your Name <you@example.com>
2024-12-20 23:11:03 +0800

    1

---
+++ 32b3d7ea-dde5-4bf7-8e1b-1e0dfe9e6bed.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "32b3d7ea-dde5-4bf7-8e1b-1e0dfe9e6bed",
+    "name": "反修 1967.9.7",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/32b3d7ea-dde5-4bf7-8e1b-1e0dfe9e6bed/1.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/32b3d7ea-dde5-4bf7-8e1b-1e0dfe9e6bed/2.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/32b3d7ea-dde5-4bf7-8e1b-1e0dfe9e6bed/3.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/32b3d7ea-dde5-4bf7-8e1b-1e0dfe9e6bed/4.png"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】反修 1967.9.7",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 7
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "32b3d7ea-dde5-4bf7-8e1b-1e0dfe9e6bed",
+  "resource_type": "book",
+  "version": 2
+}

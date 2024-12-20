8321c080b102f7b2b789e4d54e3412043908849d
Your Name <you@example.com>
2024-12-20 23:12:59 +0800

    1

---
+++ df8ddeb6-eada-4f63-b616-2a1d2c7e97ce.ts
@@ -0,0 +1,41 @@
+export default {
+  "entity": {
+    "id": "df8ddeb6-eada-4f63-b616-2a1d2c7e97ce",
+    "name": "工业批判 1967.9.12",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/df8ddeb6-eada-4f63-b616-2a1d2c7e97ce/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/df8ddeb6-eada-4f63-b616-2a1d2c7e97ce/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工业批判 1967.9.12",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 12
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "df8ddeb6-eada-4f63-b616-2a1d2c7e97ce",
+  "resource_type": "book",
+  "version": 2
+}

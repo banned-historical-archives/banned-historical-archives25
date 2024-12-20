171b436c0f16597bee5d56b611f0969512a6bfed
Your Name <you@example.com>
2024-12-20 23:17:31 +0800

    1

---
+++ 1f345361-4ea5-48e5-a445-c95f39666adc.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "1f345361-4ea5-48e5-a445-c95f39666adc",
+    "name": "风雷激 1967.1.9",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1f345361-4ea5-48e5-a445-c95f39666adc/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】风雷激 1967.1.9",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 1,
+            "day": 9
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "1f345361-4ea5-48e5-a445-c95f39666adc",
+  "resource_type": "book",
+  "version": 2
+}

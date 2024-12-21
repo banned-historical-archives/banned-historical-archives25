e2aa26a082b37b2e0c8104484a1636e97b392ccf
Your Name <you@example.com>
2024-12-21 08:13:43 +0800

    1

---
+++ 272a6d84-b5a7-4daa-b4b2-749cd5655735.ts
@@ -0,0 +1,51 @@
+export default {
+  "entity": {
+    "id": "272a6d84-b5a7-4daa-b4b2-749cd5655735",
+    "name": "红卫东 1967.2",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/272a6d84-b5a7-4daa-b4b2-749cd5655735/13.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】红卫东 1967.2",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 13,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 2
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "272a6d84-b5a7-4daa-b4b2-749cd5655735",
+  "resource_type": "book",
+  "version": 2
+}

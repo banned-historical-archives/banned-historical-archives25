db0a4f2e83faad12e7d9584b5ae56f728d4eeca1
Your Name <you@example.com>
2024-12-21 08:05:45 +0800

    1

---
+++ 1b0bbafd-f395-4558-acd5-f6039b8e1e88.ts
@@ -0,0 +1,48 @@
+export default {
+  "entity": {
+    "id": "1b0bbafd-f395-4558-acd5-f6039b8e1e88",
+    "name": "纺织工人报 1968.4.3",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1b0bbafd-f395-4558-acd5-f6039b8e1e88/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1b0bbafd-f395-4558-acd5-f6039b8e1e88/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1b0bbafd-f395-4558-acd5-f6039b8e1e88/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1b0bbafd-f395-4558-acd5-f6039b8e1e88/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1b0bbafd-f395-4558-acd5-f6039b8e1e88/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1b0bbafd-f395-4558-acd5-f6039b8e1e88/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1b0bbafd-f395-4558-acd5-f6039b8e1e88/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1b0bbafd-f395-4558-acd5-f6039b8e1e88/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/1b0bbafd-f395-4558-acd5-f6039b8e1e88/9.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】纺织工人报 1968.4.3",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 9,
+        "dates": [
+          {
+            "year": 1968,
+            "month": 4,
+            "day": 3
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "1b0bbafd-f395-4558-acd5-f6039b8e1e88",
+  "resource_type": "book",
+  "version": 2
+}

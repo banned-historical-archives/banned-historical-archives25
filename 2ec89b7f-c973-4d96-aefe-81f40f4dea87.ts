78181986f359c4938bf7880c7da79db539f1ea9b
Your Name <you@example.com>
2024-12-21 08:22:04 +0800

    1

---
+++ 2ec89b7f-c973-4d96-aefe-81f40f4dea87.ts
@@ -0,0 +1,49 @@
+export default {
+  "entity": {
+    "id": "2ec89b7f-c973-4d96-aefe-81f40f4dea87",
+    "name": "文艺战讯 1967.9.12",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/1.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/2.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/3.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/4.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/5.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/6.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/7.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/8.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/9.png",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/2ec89b7f-c973-4d96-aefe-81f40f4dea87/10.png"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】文艺战讯 1967.9.12",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 10,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9,
+            "day": 12
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "2ec89b7f-c973-4d96-aefe-81f40f4dea87",
+  "resource_type": "book",
+  "version": 2
+}

50d8544f6559c0aad649e3f1c331a6f47e994f61
Your Name <you@example.com>
2024-12-21 08:12:41 +0800

    1

---
+++ c670808b-8084-4dfb-a5b7-eac25201b906.ts
@@ -0,0 +1,39 @@
+export default {
+  "entity": {
+    "id": "c670808b-8084-4dfb-a5b7-eac25201b906",
+    "name": "工地战报 1976.5",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/c670808b-8084-4dfb-a5b7-eac25201b906/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】工地战报 1976.5",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "dates": [
+          {
+            "year": 1976,
+            "month": 5
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "c670808b-8084-4dfb-a5b7-eac25201b906",
+  "resource_type": "book",
+  "version": 2
+}

5dfe28d69728133a94a817736b71ee5b4cddb59d
Your Name <you@example.com>
2024-12-21 08:14:34 +0800

    1

---
+++ b95030ee-566c-4f17-8e54-57e89c9f8686.ts
@@ -0,0 +1,51 @@
+export default {
+  "entity": {
+    "id": "b95030ee-566c-4f17-8e54-57e89c9f8686",
+    "name": "风雷激 1967.9",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives25/main/b95030ee-566c-4f17-8e54-57e89c9f8686/13.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "【文章待拆分】风雷激 1967.9",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 13,
+        "dates": [
+          {
+            "year": 1967,
+            "month": 9
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "b95030ee-566c-4f17-8e54-57e89c9f8686",
+  "resource_type": "book",
+  "version": 2
+}
